a6d39ab5c30925d281e3cb2fb4371f3fc7ea4ae3
Oleg Chudilin <73231661+EraserH@users.noreply.github.com>
2021-11-02 14:50:59 +0400

    It seems it`s a final version

--- main.py
+++ main.py
@@ -1,53 +1,3 @@
-"""import os
-import hashlib as hl
-import zipfile as zf
-import requests as rq
-import re
-#распаковать архив и зайти в папку
-archive = zf.ZipFile("tiff-4.2.0_lab1.zip")
-archive.extractall(os.getcwd())
-os.chdir("tiff-4.2.0")
-tmp = list(os.walk("."))
-res2 = []
-#найти текстовые файлы и их хеши
-for i in tmp:
-    for fn in i[2]:
-        path = i[0] + "/" + fn
-        if ".txt" in path:
-            res2.append(fn)
-            f = open(path, "rb")
-            res2.append(hl.md5(f.read()).hexdigest())
-            f.close()
-#вывести текстовые файлы и их хеши
-for i in res2:
-    if ".txt" in i:
-        print(i, end = " ")
-    else:
-        print(i)
-#найти нужный файл и сохранить ссылку на сайт
-target = "4636f9ae9fef12ebd56cd39586d33cfb"
-link = ""
-for i in tmp:
-    for fn in i[2]:
-        path = i[0] + "/" + fn
-        f = open(path, "rb")
-        if hl.md5(f.read()).hexdigest() == target:
-            f.close()
-            f = open(path, "r")
-            link = f.read()
-        f.close()
-
-info = rq.get(link)
-lines = re.findall(r'<div class="Table-module_row__3TH83">.*?</div>.*?</div>.*?</div>.*?</div>.*?</div>', info.text)
-
-counter = 0
-#headers = []
-for line in lines:
-    if counter == 0:
-        headers = re.sub("<.*?>", " ",line)
-        headers = re.findall("Заболели|Умерли|Вылечились|Активные случаи", headers)
-        print(headers) """
-
 import zipfile
 import os
 import hashlib
@@ -58,17 +8,16 @@ import csv
 directory_to_extract_to = 'D:\\unzippedDirectory'
 arch_file = 'D:\\tiff-4.2.0_lab1.zip'
 
-
 test_zip = zipfile.ZipFile(arch_file)
 test_zip_files = test_zip.namelist()
 print(test_zip_files)
-#test_zip.extractall(directory_to_extract_to)
+test_zip.extractall(directory_to_extract_to)
 test_zip.close()
 
 #Получить список файлов (полный путь) формата txt, находящихся в directory_to_extract_to. Сохранить полученный список в txt_files
-txt_files=[]
+
+txt_files = []
 for r, d, f in os.walk(directory_to_extract_to):
-    #print(r)
     for i in f:
         if (re.match(r"txt[.].+", i[::-1])):
             txt_files.append(r + "\\" + i)
@@ -112,52 +61,43 @@ lines = re.findall(r'<div class="Table-module_row__3TH83">.*?</div>.*?</div>.*?<
 for line in lines:
     # извлечение заголовков таблицы
     if counter == 0:
-        # Удаление тегов
         headers = re.sub('<.*?>', ' ', line)
         # Извлечение списка заголовков
         headers = re.findall(r'Заболели|Умерли|Вылечились|Активные случаи', headers)
+        headers.insert(0, "Страны")
         print(headers, end="\n\n")
     else:
         if counter == 1:
             print(line, end="\n\n")
-        # Удаление тегов
-        # Значения в таблице, заключенные в скобках, не учитывать.
-        # Для этого удалить скобки и символы между ними.
-        # Замена последовательности символов ';' на одиночный символ
-        # Удаление символа ';' в начале и в конце строки
-        temp = re.sub('<.*?>', ';', line)
-        print(temp)
-        temp = re.sub("\(.*?\)", '', temp)
-        print(temp)
-        temp = re.sub(';+', ';', temp)
-        print(temp)
-        temp = temp[1: len(temp) - 1]
-        print(temp)
-        temp = re.sub('\s(?=\d)', '', temp)
-        print(temp)
-        temp = re.sub('(?<=\d)\s', '', temp)
-        print(temp)
-        temp = re.sub('(?<=0)\*', '', temp)
-        print(temp)
-        temp = re.sub('_', '-1', temp)
-        print(temp)
 
-        print("\n\n")
+        temp = re.sub('<.*?>', ';', line)
+    #    print(temp)
+        temp = re.sub("\(.*?\)", '', temp)
+    #    print(temp)
+        temp = re.sub(';+', ';', temp)
+    #    print(temp)
+        temp = temp[1: len(temp) - 1]
+    #    print(temp)
+        temp = re.sub('\s(?=\d)', '', temp)
+    #    print(temp)
+        temp = re.sub('(?<=\d)\s', '', temp)
+    #    print(temp)
+        temp = re.sub('\*', '', temp)
+    #    print(temp)
+        temp = re.sub('_', '-1', temp)
+    #    print(temp)
+    #    print("\n\n")
 
         # Разбитие строки на подстроки
         tmp_split = temp.split(';')
 
-        #print(tmp_split, len(tmp_split), sep=" ||| ")
-        if len(tmp_split) == 6:
+        if len(tmp_split) == 6: #последняя строка "всего"
             tmp_split.pop(0)
 
         # Извлечение и обработка (удаление "лишних" символов) данных из первого столбца
         country_name = tmp_split[0]
         country_name = re.sub('.*\s\s', '', country_name)
 
-        # Извлечение данных из оставшихся столбцов. Данные из этих столбцов должны иметь числовое значение
-        # (прочерк можно заменить на -1).
-        # Некоторые строки содержат пробелы в виде символа '\xa0'.
         col1_val = tmp_split[1]
         col2_val = tmp_split[2]
         col3_val = tmp_split[3]
@@ -174,7 +114,6 @@ for line in lines:
 
 # Задание №5
 # Запись данных из полученного словаря в файл
-#print(os.getcwd(), end="\n\n")
 
 output = open('data.csv', 'w')
 w = csv.writer(output, delimiter=";")
@@ -186,8 +125,8 @@ output.close()
 # Задание №6
 # Вывод данных на экран для указанного первичного ключа (первый столбец таблицы)
 
-#target_country = input("Введите название страны: ")
-#print(result_dct[target_country])
+target_country = input("Введите название страны: ")
+print(result_dct[target_country])
 
 
 """<div class="Table-module_row__3TH83"><div class="Table-module_cell__EFKDW Table-module_white__gzvo0 Table-module_m__29G9r">🇺🇸  США</div><div class="Table-module_cell__EFKDW Table-module_white__gzvo0 Table-module_s__Vl_Eg">45&nbsp;953&nbsp;186 <span style="color:#5C5E62;font-size:10px">(+29&nbsp;302)</span></div><div class="Table-module_cell__EFKDW Table-module_white__gzvo0 Table-module_s__Vl_Eg">745&nbsp;668 <span style="color:#5C5E62;font-size:10px">(+291)</span></div><div class="Table-module_cell__EFKDW Table-module_white__gzvo0 Table-module_s__Vl_Eg">0*</div><div class="Table-module_cell__EFKDW Table-module_white__gzvo0 Table-module_s__Vl_Eg">_</div></div>"""
